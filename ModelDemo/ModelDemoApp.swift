e506990f8248f37e05373de3c19fdad01d6c8bf1
tony@giaccone.org <tony@giaccone.org>
2023-08-02 08:57:34 -0400

    Empty Project

---
+++ ModelDemo/ModelDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelDemoApp.swift
+//  ModelDemo
+//
+//  Created by tony giaccone on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ModelDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
